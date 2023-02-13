2f180bbc6d7ea51f811fbbfda28afe369d23b3e5
Vikram Singh <v.p.singh@gmail.com>
2023-02-13 15:35:32 -0800

    Update server.js

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS',
   );
   next();
 });
